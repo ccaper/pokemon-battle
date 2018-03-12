386e41cdea912d3ea750597f7ffee3cb437d00bb
ccaper <christian.caperton@gmail.com>
2018-03-12 18:38:32 -0500

    adding comments

--- utils/getApiData.js
+++ utils/getApiData.js
@@ -1,5 +1,14 @@
 const axios = require('axios');
 
+/*
+* The pokemon API is flakey (getting 502 gateway timeout errors) so I wrapped
+*    the calls in retries that tries 3 times before failing.
+*
+* url: the fully qualified URL to fetch API data
+*
+* returns: a promise that resolves to the data or rejects with the failure status
+*    code.
+*/
 function getApiDataWithRetries(url) {
   return new Promise(async (resolve, reject) => {
     let apiAttempt = 0;
